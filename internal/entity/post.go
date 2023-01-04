49d278473aed72f59b8341233b32ffa833f92d8d
Mirat Rafikov <miratworking@gmail.com>
2023-01-04 10:35:30 +0300

    use json marshaling

--- internal/entity/post.go
+++ internal/entity/post.go
@@ -1,10 +1,24 @@
 package entity
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Post struct {
 	Id        int32     `json:"id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"createdAt"`
+}
+
+func DeserializePosts(rows *sql.Rows) []Post {
+	posts := []Post{}
+	for rows.Next() {
+		post := Post{}
+		rows.Scan(&post.Id, &post.Title, &post.Content, &post.CreatedAt)
+		posts = append(posts, post)
+	}
+
+	return posts
 }
--- internal/usecase/get_all_posts.go
+++ internal/usecase/get_all_posts.go
@@ -1,42 +1,27 @@
 package usecase
 
 import (
-	"database/sql"
-	"fmt"
+	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/miratrafikov/blog_backend/internal/db"
+	"github.com/miratrafikov/blog_backend/internal/entity"
 )
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	posts, err := db.Connection.Query("SELECT * FROM post;")
+	rows, err := db.Connection.Query("SELECT * FROM post")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error getting the posts"))
+		w.Write([]byte(err.Error()))
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(postsToPseudoJson(posts)))
-}
-
-func postsToPseudoJson(posts *sql.Rows) string {
-	postElements := ""
-	i := 0
-	for posts.Next() {
-		var id int
-		var title string
-		var content string
-		var createdAt time.Time
-		posts.Scan(&id, &title, &content, &createdAt)
-		if i > 0 {
-			postElements += ",\n"
-		}
-
-		postElements += fmt.Sprintf("  {\n    id: %d,\n    title: %s,\n    content: %s,\n    createdAt: %d\n  }", id, title, content, createdAt.Unix())
-		i++
+	posts := entity.DeserializePosts(rows)
+	json, err := json.Marshal(posts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json)
 	}
-
-	return fmt.Sprintf("[\n%s\n]", postElements)
 }
