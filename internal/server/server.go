6bb717697c1b92f154df34955f4ec89f3bfc3dd6
Mirat Rafikov <miratworking@gmail.com>
2023-01-04 15:22:23 +0300

    routing

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/miratrafikov/blog-backend/internal/controller"
 )
@@ -13,14 +14,50 @@ type StartServerInput struct {
 	DatabaseName string
 }
 
+type routeMapping struct {
+	Prefix     string
+	Controller func(http.ResponseWriter, *http.Request)
+}
+
+var routeMappings = []routeMapping{
+	{
+		Prefix:     "/posts/",
+		Controller: controller.RoutePosts,
+	},
+}
+
 func StartApplication(input StartServerInput) {
-	assignControllers()
+	http.HandleFunc("/", route)
 	serverAddress := fmt.Sprintf("localhost:%s", input.ServerPort)
 	if err := http.ListenAndServe(serverAddress, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func assignControllers() {
-	http.HandleFunc("/posts", controller.RoutePosts)
+func route(w http.ResponseWriter, r *http.Request) {
+	url := getUrl(r)
+	for _, mapping := range routeMappings {
+		if strings.HasPrefix(url, mapping.Prefix) {
+			mapping.Controller(w, r)
+
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func getUrl(r *http.Request) string {
+	url := r.URL.String()
+	url = ensureEndsWithSlash(url)
+
+	return url
+}
+
+func ensureEndsWithSlash(url string) string {
+	if url[len(url)-1:] != "/" {
+		return url + "/"
+	}
+
+	return url
 }
